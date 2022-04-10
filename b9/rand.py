dce5fbc2420f61a9219cf64d85f9fe9d93a23581
tanya546 <68525106+tanya546@users.noreply.github.com>
2022-04-09 17:09:50 -0700

    Update rand.py

--- b9/rand.py
+++ b9/rand.py
@@ -61,7 +61,10 @@ def roll_dice(iterations, sides):
 
 
     # COMMIT 2: Add dice rolling logic and output dice sum and sequence
+    diceSum = random.randint(1, iterations * 6)
 
+    print("{} roll(s) of a {}-sided die resulted in a sum of {}:"
+            .format(iterations, sides, diceSum))
 
     return
 
